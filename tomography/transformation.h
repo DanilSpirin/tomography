137f22008024033addf5cdd26608dc224904e499
Даниил Спирин <imaginary@imaginary-air.local>
2014-10-14 19:27:22 +0400

    delete unnecessary stuff, add virtual destructor

--- tomography/transformation.h
+++ tomography/transformation.h
@@ -16,6 +16,7 @@ class CoordinateTransformation {
 public:
     virtual void forward(point &R) = 0;
     virtual void backward(point &R) = 0;
+    virtual ~CoordinateTransformation();
 };
 
 class DecartToGeographic : public CoordinateTransformation {
