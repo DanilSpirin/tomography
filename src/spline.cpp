da964e14351df36794e0dbfbf0d58ab9c709eaa9
Daniel Spirin <l1ks.sds@gmail.com>
2017-05-23 11:52:06 +0300

    Reduce variables scope (cppcheck)

--- src/spline.cpp
+++ src/spline.cpp
@@ -2,7 +2,7 @@
 #include <limits>
 #include "spline.h"
 
-Spline::Spline() : splines (NULL) {
+Spline::Spline() : splines(nullptr) {
 }
 
 Spline::~Spline() {
@@ -11,7 +11,7 @@ Spline::~Spline() {
 
 void Spline::freeMemory() {
     delete[] splines;
-    splines = NULL;
+    splines = nullptr;
 }
 
 void Spline::build(const double *x, const double *y, std::size_t n) {
@@ -26,21 +26,19 @@ void Spline::build(const double *x, const double *y, std::size_t n) {
     }
 
     // Граничные условия
-    splines[0].c = splines[n-1].c = 0;
+    splines[0].c = splines[n - 1].c = 0;
 
     // Метод прогонки
     // Нахождение прогоночных коэффициентов
     double *alpha = new double[n - 1];
     double *beta = new double[n - 1];
-    double A, B, C, F, h_i, h_i1, z;
     alpha[0] = beta[0] = 0.;
     for (std::size_t i = 1; i < n - 1; ++i) {
-        h_i = x[i] - x[i - 1], h_i1 = x[i + 1] - x[i];
-        A = h_i;
-        C = 2.0 * (h_i + h_i1);
-        B = h_i1;
-        F = 6.0 * ((y[i + 1] - y[i]) / h_i1 - (y[i] - y[i - 1]) / h_i);
-        z = (A * alpha[i - 1] + C);
+        const double A = x[i] - x[i - 1];
+        const double B = x[i + 1] - x[i];
+        const double C = 2.0 * (A + B);
+        const double F = 6.0 * ((y[i + 1] - y[i]) / B - (y[i] - y[i - 1]) / A);
+        const double z = A * alpha[i - 1] + C;
         alpha[i] = -B / z;
         beta[i] = (F - A * beta[i - 1]) / z;
     }
@@ -55,13 +53,13 @@ void Spline::build(const double *x, const double *y, std::size_t n) {
 
     // Нахождение оставшихся коэффициентов
     for (std::size_t i = n - 1; i > 0; --i) {
-        double h_i = x[i] - x[i - 1];
-        splines[i].d = (splines[i].c - splines[i - 1].c) / h_i;
-        splines[i].b = h_i * (2.0 * splines[i].c + splines[i - 1].c) / 6.0 + (y[i] - y[i - 1]) / h_i;
+        const double h = x[i] - x[i - 1];
+        splines[i].d = (splines[i].c - splines[i - 1].c) / h;
+        splines[i].b = h * (2.0 * splines[i].c + splines[i - 1].c) / 6.0 + (y[i] - y[i - 1]) / h;
     }
 }
 
-double Spline::operator()(double x) {
+double Spline::operator()(const double x) const {
     if (!splines)
         return std::numeric_limits<double>::quiet_NaN();
 
@@ -69,7 +67,7 @@ double Spline::operator()(double x) {
     if (x <= splines[0].x) { // Если х меньше левой границы то возвращаем значение на левой границе
         return splines[0].a;
     } else if (x >= splines[n - 1].x) { // Аналогично с правой
-        return splines[n-1].a;
+        return splines[n - 1].a;
     } else { // Иначе проводим бинарный поиск нужного элемента массива
         std::size_t i = 0, j = n - 1;
         while (i + 1 < j) {
@@ -83,7 +81,7 @@ double Spline::operator()(double x) {
         s = splines + j;
     }
 
-    double dx = (x - s->x);
+    const double dx = (x - s->x);
 
     return s->a + (s->b + (s->c / 2.0 + s->d * dx / 6.0) * dx) * dx;
 }
--- src/spline.h
+++ src/spline.h
@@ -5,7 +5,7 @@ public:
     Spline();
     ~Spline();
     void build(const double *x, const double *y, std::size_t n); // построение сплайна по n точкам (x, y)
-    double operator() (double x); // возвращает значение в точке x
+    double operator() (const double x) const; // возвращает значение в точке x
 private:
     //структура, описывающая сплайн на каждом сегменте сетки
     struct SplineTuple{
