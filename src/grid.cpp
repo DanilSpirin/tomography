5a16aab0439a31b70a28f3f28439debf1ea2e59a
Daniel Spirin <l1ks.sds@gmail.com>
2018-02-08 02:31:04 +0300

    Extract spline creation out of Grid::basis

--- src/grid.cpp
+++ src/grid.cpp
@@ -5,6 +5,10 @@
 #include "spline.h"
 #include "tools.h"
 
+const std::vector<double> spline_y = {0, 0.25, 1, 0.25, 0};
+const std::vector<double> spline_x = {-2, -1, 0, 1, 2};
+const Spline spline(spline_x, spline_y);
+
 void Grid::set(const Dimension &latitude, const Dimension &longitude, const Dimension &time) {
     clear();
     this->latitude = latitude;
@@ -19,10 +23,6 @@ void Grid::set(const Dimension &latitude, const Dimension &longitude, const Dime
 
 VectorSparse Grid::basis(const double x, const double y, const double z) const {
     VectorSparse basis_vector;
-
-    const std::vector<double> spline_y = {0, 0.25, 1, 0.25, 0};
-    const std::vector<double> spline_x = {-2, -1, 0, 1, 2};
-    Spline spline(spline_x, spline_y);
 
     for (const auto& lat : this->latitude.sequence(x)) {
         for (const auto& lon : this->longitude.sequence(y)) {
